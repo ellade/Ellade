cfecd1716a41e8db697500799e3f3d175d96942d
Roberto <166568@studenti.unimore.it>
2016-03-19 00:20:42 +0100

    ok

--- ElladeServer/src/Main.java
+++ ElladeServer/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			
+			System.setSecurityManager(new RMISecurityManager());
 
 			ElladeServer srv=new ElladeServer();
 			
